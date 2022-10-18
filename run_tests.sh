57d0e35fa6d1b0528c2d749a1aeb5401ab58c4d0
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-10-18 14:34:22 -0400

    adjustments

--- run_tests.sh
+++ run_tests.sh
@@ -7,14 +7,12 @@ docker run \
     -c "npm update -g && npm install -g newman-reporter-htmlextra;
         newman run /etc/newman/cwms-radar-api.postman_collection.json \
         --environment=/etc/newman/postman_environment.radar_cpc_production.json \
-        -r htmlextra --reporter-htmlextra-noSyntaxHightlighting \
-        --reporter-htmlextra-export --reporter-htmlextra-noSyntaxHightlighting \
+        --reporters cli,htmlextra \
         --reporter-htmlextra-template /etc/newman/dashboard-template.hbs \
         --reporter-htmlextra-browserTitle 'CWMS RADAR Results' \
         --reporter-htmlextra-title 'CWMS RADAR Results' \
         --reporter-htmlextra-titleSize '4' \
         --reporter-htmlextra-export /output/index.html \
         --suppress-exit-code"
-
 
 # docker build -t postman/newman . && docker run -v $(pwd)/tests:/etc/newman -v $(pwd)/docs:/output postman/newman
