3dbf5af7d4c1efed20476b84627977d4191e4ed2
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-10-18 17:45:24 -0400

    update script

--- run_tests.sh
+++ run_tests.sh
@@ -8,9 +8,9 @@ do
     esac
 done
 if [ "$env" = "local" ]; then
-    ENV_CONFIG="/etc/newman/postman_environment.radar_local.json"
+    ENV_CONFIG="postman_environment.radar_local.json"
 else
-    ENV_CONFIG="/etc/newman/postman_environment.radar_cpc_production.json"
+    ENV_CONFIG="postman_environment.radar_cpc_production.json"
 fi
 echo "Using postman env file: $ENV_CONFIG";
 
@@ -20,7 +20,7 @@ docker run \
     --entrypoint /bin/bash postman/newman:ubuntu \
     -c "npm update -g && npm install -g newman-reporter-htmlextra;
         newman run /etc/newman/cwms-radar-api.postman_collection.json \
-        --environment=${ENV_CONFIG} \
+        --environment=/etc/newman/${ENV_CONFIG} \
         --reporters cli,htmlextra \
         --reporter-htmlextra-template /etc/newman/dashboard-template.hbs \
         --reporter-htmlextra-browserTitle 'CWMS RADAR Results' \
