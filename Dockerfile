d762ca6af40e940eb8319c7b74ba89c1439ad6a2
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-10-17 22:21:07 -0400

    another try

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,20 @@
-FROM postman/newman:alpine
+# FROM postman/newman:ubuntu
 
 
 
-# Cache buster
-RUN echo $(date +%s)
+# # Cache buster
+# RUN echo $(date +%s)
 
-RUN npm install -g newman-reporter-htmlextra
+# RUN npm update -g && install -g newman-reporter-htmlextra
 
-WORKDIR /etc/newman
+# WORKDIR /etc/newman
 
-ENTRYPOINT [ "newman", "run", "cwms-radar-api.postman_collection.json", \
-            "--environment=postman_environment.radar_cpc_production.json", \
-            "-r", "htmlextra", \
-            "--reporter-htmlextra-noSyntaxHightlighting", \
-            "--reporter-htmlextra-template", "/etc/newman/dashboard-template.hbs", \
-            "--reporter-htmlextra-browserTitle", "CWMS RADAR Results", \
-            "--reporter-htmlextra-title", "CWMS RADAR Results", \
-            "--reporter-htmlextra-titleSize", "4", \
-            "--reporter-htmlextra-export", "/output/index.html"]
-
-
-
-# ENTRYPOINT ["tail", "-f", "/dev/null"]
+# ENTRYPOINT [ "newman", "run", "cwms-radar-api.postman_collection.json", \
+#             "--environment=postman_environment.radar_cpc_production.json", \
+#             "-r", "htmlextra", \
+#             "--reporter-htmlextra-noSyntaxHightlighting", \
+#             "--reporter-htmlextra-template", "/etc/newman/dashboard-template.hbs", \
+#             "--reporter-htmlextra-browserTitle", "CWMS RADAR Results", \
+#             "--reporter-htmlextra-title", "CWMS RADAR Results", \
+#             "--reporter-htmlextra-titleSize", "4", \
+#             "--reporter-htmlextra-export", "/output/index.html"]
--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,19 @@
 # Run Tests
 docker network create --driver bridge radar-api_default || true
 
-# docker run \
-#     -v $(pwd)/tests:/etc/newman --network=radar-api_default \
-#     --entrypoint /bin/bash postman/newman:ubuntu \
-#     -c "npm i -g newman-reporter-htmlextra;
-#         newman run /etc/newman/cwms-radar-api.postman_collection.json \
-#         --environment=/etc/newman/postman_environment.radar_cpc_production.json \
-#         -r htmlextra --reporter-htmlextra-export --reporter-htmlextra-noSyntaxHightlighting \
-#         /etc/newman/radar-api-results.html"
+docker run \
+    -v $(pwd)/tests:/etc/newman -v $(pwd)/docs:/output  --network=radar-api_default \
+    --entrypoint /bin/bash postman/newman:ubuntu \
+    -c "npm update -g && npm install -g newman-reporter-htmlextra;
+        newman run /etc/newman/cwms-radar-api.postman_collection.json \
+        --environment=/etc/newman/postman_environment.radar_cpc_production.json \
+        -r htmlextra --reporter-htmlextra-noSyntaxHightlighting \
+        --reporter-htmlextra-export --reporter-htmlextra-noSyntaxHightlighting \
+        --reporter-htmlextra-template /etc/newman/dashboard-template.hbs \
+        --reporter-htmlextra-browserTitle, 'CWMS RADAR Results', \
+        --reporter-htmlextra-title 'CWMS RADAR Results' \
+        --reporter-htmlextra-titleSize '4', \
+        --reporter-htmlextra-export /output/index.html"
 
-docker build -t postman/newman . && docker run -v $(pwd)/tests:/etc/newman -v $(pwd)/docs:/output postman/newman
+
+# docker build -t postman/newman . && docker run -v $(pwd)/tests:/etc/newman -v $(pwd)/docs:/output postman/newman
